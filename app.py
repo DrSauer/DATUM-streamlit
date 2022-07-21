6b2346c037f8a55a5c92401d2218a22c503c8de2
Kenneth Yap <85454120+kenneth-yap@users.noreply.github.com>
2022-07-21 11:25:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ pd.options.mode.chained_assignment = None
 st.set_page_config(layout="wide")
 st.title('Data Analysis & Tunnel Management (DATuM) Dashboard')
 
-url = 'https://github.com/kenneth-yap/DATUM-streamlit/README.md'
+url = 'https://github.com/kenneth-yap/DATUM-streamlit/blob/main/README.md'
 st.write('READ ME [link](%s)' % url)
 
 # Upload file
